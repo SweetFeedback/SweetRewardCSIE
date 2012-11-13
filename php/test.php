48a87dadd63808e0a632927cac9c55f452144e12
Louis Yu <louis29418401@gmail.com>
2012-11-13 21:15:11 +0800

    revise the version to gumball server newest version

--- php/test.php
+++ php/test.php
@@ -1,14 +1,16 @@
 <?php
 require_once("db.php");
 
-$db = new DB();
-if( isset($_GET["account"]) && isset($_GET["password"])){
-    $username = $_GET["account"];
-    $password = $_GET["password"];
-    $db = new DB();
-    $result = $db->verifyUser($username, $password);
-    echo json_encode($result);
-}
+$db = new DB(Config::read('db.host.gardenia'), Config::read('db.basename.gardenia'), Config::read('db.user.gardenia'), Config::read('db.password.gardenia'));
+print_r($db->getExistReport());
+#$db = new DB();
+#if( isset($_GET["account"]) && isset($_GET["password"])){
+#    $username = $_GET["account"];
+#    $password = $_GET["password"];
+#    $db = new DB();
+#    $result = $db->verifyUser($username, $password);
+#    echo json_encode($result);
+#}
 
 //echo $db->getNumberOfOnlineUser();
 //echo $db->getMaxTimeStamp();
