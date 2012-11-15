f8b42ddc69192f818b38ad2a8bc3b07339daf858
Louis Yu <louis29418401@gmail.com>
2012-11-15 15:45:15 +0800

    fix typo

--- php/getExistReports.php
+++ php/getExistReports.php
@@ -5,7 +5,7 @@ require_once("db.php");
 $db = DB::getInstance(Config::read('db.host'), Config::read('db.basename'), Config::read('db.user'), Config::read('db.password'));
 if(isset($_GET["room_id"])){
     $room_id = $_GET["room_id"];
-    $reuslt = $db->getRoomExistReport($room_id);
+    $result = $db->getRoomExistReport($room_id);
 }else{
     $result = $db->getExistReport();
 }
