efc52a61733d7c0a380d162e35d1bb0d78b3ed11
Louis Yu <louis29418401@gmail.com>
2012-12-13 16:55:29 +0800

    implement policy agent

---
+++ php/policyAgent.php
@@ -0,0 +1,31 @@
+<?php
+class PolicyAgent{
+    public function __construct(){
+        date_default_timezone_set("Asia/Chongqing"); 
+    } 
+
+    public function judgeWindowStatus($window_status){
+        $date = new DateTime();
+        #echo $date->getTimeStamp();
+        $hour = date('H', $date->getTimeStamp());
+        if( $hour >= 8 && $hour <= 20) {
+            // day time 
+            if( $window_status == 1 ){
+                return false;
+            }
+            else{
+                return true;
+            }
+        }
+        else{
+            // night time 
+            if( $window_status == 1 ){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+    } 
+}
+?>
