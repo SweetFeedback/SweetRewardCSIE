b71098929169569f790b70e73e0dbb3aece06006
Louis Yu <louis29418401@gmail.com>
2012-12-26 01:34:20 +0800

    rank the update problem rank list

---
+++ php/getSolveProblemRank.php
@@ -0,0 +1,9 @@
+<?
+require_once("db.php");
+$db = DB::getInstance(Config::read('db.host'), Config::read('db.basename'), Config::read('db.user'), Config::read('db.password'));
+$result = $db->getRankSolveProblemUser();
+if($result != null){
+    echo json_encode($result);
+}
+    
+?>
