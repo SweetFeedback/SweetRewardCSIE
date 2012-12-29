f10758cac9f76cf358925bf358bdee485aa2ee5d
Louis Yu <louis29418401@gmail.com>
2012-12-29 19:15:14 +0800

    if get action key value, dont get the window data from the server

--- php/userActionTrigger.php
+++ php/userActionTrigger.php
@@ -34,52 +34,53 @@ if(isset($_GET['window_id']) && isset($_GET['token']) ){
                 else if($window_id == 1 || $window_id == 2){
                     $db->insertFeedbackStatusByDeviceId(3, 11, "positive", "open the window at right time");
                 }
-                #$db->insertFeedbackStatusByDeviceId(1, 11, "positive", "open the window");
             }
         }
         else{
             $ret['error'] = 1;
         }
     }
-    $result = $db->getNewestExtendedWindowState($window_id);
-    if($result != null){
-        $state = $result[0]['state'];
-        if($agent->judgeWindowStatus($state)){
-            #print "the state is correct now";
-            $ret = $db->getPeriodExtendedWindowState(1 - $state);
-            if($ret != null){
-                if($state == 0){
+    else{
+        $result = $db->getNewestExtendedWindowState($window_id);
+        if($result != null){
+            $state = $result[0]['state'];
+            if($agent->judgeWindowStatus($state)){
+                #print "the state is correct now";
+                $ret = $db->getPeriodExtendedWindowState(1 - $state);
+                if($ret != null){
+                    if($state == 0){
 
-                    if($window_id == 8 || $window_id == 9 || $window_id == 10){
-                        $db->insertFeedbackStatusByDeviceId(1, 11, "positive", "close the window at right time");
+                        if($window_id == 8 || $window_id == 9 || $window_id == 10){
+                            $db->insertFeedbackStatusByDeviceId(1, 11, "positive", "close the window at right time");
+                        }
+                        else if($window_id == 11 || $window_id == 12 || $window_id == 13){
+                            $db->insertFeedbackStatusByDeviceId(2, 11, "positive", "close the window at right time");
+                        }
+                        else if($window_id == 1 || $window_id == 2){
+                            $db->insertFeedbackStatusByDeviceId(3, 11, "positive", "close the window at right time");
+                        }
                     }
-                    else if($window_id == 11 || $window_id == 12 || $window_id == 13){
-                        $db->insertFeedbackStatusByDeviceId(2, 11, "positive", "close the window at right time");
+                    else if($state == 1){
+                        if($window_id == 8 || $window_id == 9 || $window_id == 10){
+                            $db->insertFeedbackStatusByDeviceId(1, 11, "positive", "open the window at right time");
+                        }
+                        else if($window_id == 11 || $window_id == 12 || $window_id == 13){
+                            $db->insertFeedbackStatusByDeviceId(2, 11, "positive", "open the window at right time");
+                        }
+                        else if($window_id == 1 || $window_id == 2){
+                            $db->insertFeedbackStatusByDeviceId(3, 11, "positive", "open the window at right time");
+                        }
+                        #$db->insertFeedbackStatusByDeviceId(1, 11, "positive", "open the window");
                     }
-                    else if($window_id == 1 || $window_id == 2){
-                        $db->insertFeedbackStatusByDeviceId(3, 11, "positive", "close the window at right time");
-                    }
-                }
-                else if($state == 1){
-                    if($window_id == 8 || $window_id == 9 || $window_id == 10){
-                        $db->insertFeedbackStatusByDeviceId(1, 11, "positive", "open the window at right time");
-                    }
-                    else if($window_id == 11 || $window_id == 12 || $window_id == 13){
-                        $db->insertFeedbackStatusByDeviceId(2, 11, "positive", "open the window at right time");
-                    }
-                    else if($window_id == 1 || $window_id == 2){
-                        $db->insertFeedbackStatusByDeviceId(3, 11, "positive", "open the window at right time");
-                    }
-                    #$db->insertFeedbackStatusByDeviceId(1, 11, "positive", "open the window");
                 }
             }
+            else if(!$agent->judgeWindowStatus($state)){
+                $ret['error'] = "1";
+            }
         }
-        else if(!$agent->judgeWindowStatus($state)){
-            $ret['error'] = "1";
+        else{
+            $ret['no_data'] = "1";
         }
-    }
-    else{
-        $ret['no_data'] = "1";
     }
     echo json_encode($ret);
 }
