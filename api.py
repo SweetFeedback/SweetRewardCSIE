58e308fd68d8f27a3e75c87f6fb5a23cfd1aee53
Louis Yu <louis29418401@gmail.com>
2013-05-29 16:01:20 +0800

    report-related api

--- api.py
+++ api.py
@@ -4,7 +4,7 @@ from tasks import add
 
 from flask.ext.sqlalchemy import SQLAlchemy
 from sqlalchemy import Table, Column, Integer, String, Date, Float, TIMESTAMP, desc
-from model import app, db
+from model import app, db, Problem
 import config
 
 #blueprint
@@ -12,15 +12,52 @@ api = Blueprint('api', __name__)
 
 @api.route("/reports")
 def show_reports():
-	from model import Problem
 	problems = Problem.query.all()
+	return jsonify(data=[i.serialize for i in problems])
+
+@api.route("/reports/solved")
+def show_reports_unsolved():
+	problems = Problem.query.filter(Problem.status == 0).all()
+	return jsonify(data=[i.serialize for i in problems])
+
+@api.route("/reports/unsolved")
+def show_reports_unsolved():
+	problems = Problem.query.filter(Problem.status == 1).all()
+	return jsonify(data=[i.serialize for i in problems])
+
+@api.route("/reports/location/<room_id>")
+def show_reports_room(room_id):
+	problems = Problem.query.filter(Problem.room_id == room_id).all()
 	return jsonify(data=[i.serialize for i in problems])
 
 @api.route("/reports/insert", methods=['GET', 'POST'])
 def insert_report():
-	from model import Problem 
 	title = request.args.get("title", "")
 	content = request.args.get("content", "")
+	coor_x = request.args.get("coor_x", 0)
+	coor_y = request.args.get("coor_y", 0)
+	user_id = request.args.get("user_id", 0)
+	category = request.args.get("category", 0)
+	room_id = request.args.get("room_id", 0)
+
+	report = Problem(category, room_id, title, content, coor_x, coor_y, user_id)
+	db.session.add(report)
+	db.session.commit()
+
+	return jsonify(data=[report.serialize], success=1)
+
+@api.route("/reports/update/<report_id>", methods=['GET', 'POST'])
+def update_report(report_id):	
+	user_id = request.args.get("user_id", 0)
+	target_problem = Problem.query.filter_by(problem_id = report_id).first()
+	if target_problem is not None:
+		target_problem.status = 1
+		target_problem.updated_by = user_id
+		db.session.commit()
+		return_result = jsonify(data=[target_problem.serialize])
+	else:
+		return_result = jsonify(error=["can't find this problem from database"])
+	return return_result
 
 @api.route("/members")
 def list_members():
@@ -39,7 +76,8 @@ def insert_window_log():
 	window_log = Window(location_id, window_id, state)
 	db.session.add(window_log)
 	db.session.commit()
-	
+
+	# insert to window index table 	
 	indexs = db.session.query(WindowIndex).filter_by(window_id=window_id).first()
 	print indexs
 	if indexs is None:
