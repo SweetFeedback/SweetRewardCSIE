abf1ce44662bac4105c3232f857344566e1f2b56
余士元 <louisccc@yoshimototekiMacBook-Air.local>
2013-07-25 14:27:22 -0700

    people around

--- api.py
+++ api.py
@@ -344,8 +344,65 @@ def bluetooth_around():
 
 @api.route("/people_around", methods=['GET'])
 def people_around(): 
-	problem = Problem.query.filter(Problem.status == 0).first()
-	return jsonify(data={"problem": problem.serialize, "time_limit": 40})
+	#problem = Problem.query.filter(Problem.status == 0).first()
+	import json 
+	import urllib2
+	data = json.load(urllib2.urlopen('http://cmu-sensor-network.herokuapp.com/lastest_readings_from_all_devices/light/json'))
+	problems = []
+	for row in data:
+		if row['device_id'] == "test" or row['device_id'] == "test-device":
+			data.remove(row)
+	for row in data: 
+		row_hour = datetime.fromtimestamp(row['timestamp']/1000).hour
+		if row['value'] > 800 and mapping_table.has_key(row['device_id']) and (row_hour >= 21 or row_hour <= 7):
+			#print row['timestamp']
+			print datetime.fromtimestamp(row['timestamp']/1000).hour
+			#print row['value']
+			problems.append(row)
+	##print data
+	from random import choice
+	problem_choosed = choice(problems)
+	if problem_choosed != None:
+		print mapping_table[problem_choosed['device_id']][2]	
+	else:
+		print "no problem here"
+	#return jsonify(data={"problem": problem.serialize})
+	return jsonify(problem={"location": mapping_table[problem_choosed['device_id']][2], 
+							"problem_description": "light is not closing now @@! could you help me to close it?",
+							"problem_id":123,
+							"timestamp": datetime.now()})
+
+mapping_table = {
+"10170004": ["Firefly_v3","SensorAndrew0","B23.110"],
+"10170202": ["Firefly_v3","SensorAndrew2","B23.216"],
+"10170302": ["Firefly_v3","SensorAndrew3","B23.105B"],
+"10170206": ["Firefly_v3","SensorAndrew2","B23.215B"],
+"10170106": ["Firefly_v3","SensorAndrew1","B23.229"],
+"10170306": ["Firefly_v3","SensorAndrew3","B23.124"],
+"10170305": ["Firefly_v3","SensorAndrew3","B23.126"],
+"10170002": ["Firefly_v3","SensorAndrew0","B23.115"],
+"10170103": ["Firefly_v3","SensorAndrew1","B23.129"],
+"10170303": ["Firefly_v3","SensorAndrew3","B23.104"],
+"10170003": ["Firefly_v3","SensorAndrew0","B23.116"],
+"10170102": ["Firefly_v3","SensorAndrew1","B23.129A"],
+"10170104": ["Firefly_v3","SensorAndrew1","B23.230"],
+"10170005": ["Firefly_v3","SensorAndrew0","B23.109"],
+"10170307": ["Firefly_v3","SensorAndrew3","B23.122"],
+"10170007": ["Firefly_v3","SensorAndrew0","B23.211"],
+"10170304": ["Firefly_v3","SensorAndrew3","B23.123"],
+"10170204": ["Firefly_v3","SensorAndrew2","B23.214"],
+"10170203": ["Firefly_v3","SensorAndrew2","B23.213"],
+"10170208": ["Firefly_v3","SensorAndrew2","B23.217A"],
+"10170008": ["Firefly_v3","SensorAndrew0","B23.212"],
+"10170009": ["Firefly_v3","SensorAndrew0","B23.210"],
+"10170207": ["Firefly_v3","SensorAndrew2","B23.215"],
+"10170308": ["Firefly_v3","SensorAndrew3","B23.120"],
+"10170209": ["Firefly_v3","SensorAndrew2","B23.217B"],
+"10170006": ["Firefly_v3","SensorAndrew0","B23.107"],
+"10170205": ["Firefly_v3","SensorAndrew2","B23.214B"],
+"10170105": ["Firefly_v3","SensorAndrew1","B23.228"]
+}
+
 @api.route("/check_problem", methods=['GET'])
 def check_problem(): 
 	problem_id = request.args.get("problem_id", -1)
