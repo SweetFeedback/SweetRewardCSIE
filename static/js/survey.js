ba6168672df9581580d79f80707781aca716a873
catlikethief <a50119729@gmail.com>
2013-09-11 16:38:59 -0400

    All survey question must be answered.

--- static/js/survey.js
+++ static/js/survey.js
@@ -3,20 +3,8 @@ var refreshFlag = 0;
 $("#submit").click(function() {
 	var question1 = $('input[name="group1"]:checked').val();
 	var question2 = $('input[name="group2"]:checked').val();
-	/*
-	var question3 = new Array();
-	$.each($('input[name="group3"]:checked'), function(){
-		question3.push($(this).val());
-	});
-
-	var question4 = new Array();
-	$.each($('input[name="group4"]:checked'), function(){
-		question4.push($(this).val());
-	});
-*/
 	var question3 = $('input[name="group3"]:checked').val();
 	var question4 = $('input[name="group4"]:checked').val();
-
 	var question5 = $('input[name="group5"]:checked').val();
 	var question6 = $('input[name="group6"]:checked').val();
 	var question7 = $('input[name="group7"]:checked').val();
@@ -24,6 +12,66 @@ $("#submit").click(function() {
 	var question9 = $('input[name="group9"]:checked').val();
 	var question10 = $('input[name="group10"]:checked').val();
 	var question11 = $('#feeling').val();
+
+	if(typeof question1 == 'undefined') {
+		$("#error_message").text("Please answer question1");
+		$("#error_message").show();
+		$('html, body').animate({scrollTop: '400px'}, 800);
+		return;
+	} else if(typeof question2 == 'undefined') {
+		$("#error_message").text("Please answer question2");
+		$("#error_message").show();
+		$('html, body').animate({scrollTop: '400px'}, 800);
+		return;
+	} else if(typeof question3 == 'undefined') {
+		$("#error_message").text("Please answer question3");
+		$("#error_message").show();
+		$('html, body').animate({scrollTop: '400px'}, 800);
+		return;
+	} else if(typeof question4 == 'undefined') {
+		$("#error_message").text("Please answer question4");
+		$("#error_message").show();
+		$('html, body').animate({scrollTop: '400px'}, 800);
+		return;
+	} else if(typeof question5 == 'undefined') {
+		$("#error_message").text("Please answer question5");
+		$("#error_message").show();
+		$('html, body').animate({scrollTop: '400px'}, 800);
+		return;
+	} else if(typeof question6 == 'undefined') {
+		$("#error_message").text("Please answer question6");
+		$("#error_message").show();
+		$('html, body').animate({scrollTop: '400px'}, 800);
+		return;
+	} else if(typeof question7 == 'undefined') {
+		$("#error_message").text("Please answer question7");
+		$("#error_message").show();
+		$('html, body').animate({scrollTop: '400px'}, 800);
+		return;
+	} else if(typeof question8 == 'undefined') {
+		$("#error_message").text("Please answer question8");
+		$("#error_message").show();
+		$('html, body').animate({scrollTop: '400px'}, 800);
+		return;
+	} else if(typeof question9 == 'undefined') {
+		$("#error_message").text("Please answer question9");
+		$("#error_message").show();
+		$('html, body').animate({scrollTop: '400px'}, 800);
+		return;
+	} else if(typeof question10 == 'undefined') {
+		$("#error_message").text("Please answer question10");
+		$("#error_message").show();
+		$('html, body').animate({scrollTop: '400px'}, 800);
+		return;
+	} else if(question11 == '') {
+		$("#error_message").text("Please answer question11");
+		$("#error_message").show();
+		$('html, body').animate({scrollTop: '400px'}, 800);
+		return;
+	}
+
+	$("#error_message").hide();
+	
 
 	var data = {
 		'question1': question1,
@@ -62,5 +110,6 @@ $(document).mousemove(function(event){
 });
 
 $(function() {
+	$("#error_message").hide();
 	setInterval(reload, 30000);
 });
